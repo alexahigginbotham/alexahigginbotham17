064614c1b94fe79fa7e59bd1e38b6bcc3b80857b
alexahigginbotham <sunshineshamrock3000@gmail.com>
2026-09-20 16:56:55 -0500

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -85,10 +85,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
 });
-<a href="index.html" class="site-logo" aria-label="Alexa Higginbotham Home">
-    <span class="logo-circle">
-        <span class="logo-a">A</span>
-        <span class="logo-h">H</span>
-        <span class="logo-sparkle">✦</span>
-    </span>
-</a>
